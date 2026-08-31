c7dfadad20303b59278dbc1ded9235e621e15b70
Юрий Коробаев <budimir@bk.ru>
2026-08-31 18:05:06 +0300

    мсправлена логика запроса на закрытие заявки (assigned_ope

--- src/main/java/org/grandparents/service/BotService.java
+++ src/main/java/org/grandparents/service/BotService.java
@@ -241,7 +241,12 @@ public class BotService {
                             forward.addButtonFullRow("🏠 Главное меню", "main_menu");
 
                             messageSender.sendMessage(recipient.getChatId(), forward);
+
                             log.info("📨 [MESSAGE] Сообщение отправлено пользователю {}", recipientId);
+
+                            // Запоминаем, что получатель теперь ведёт чат по этой заявке
+                            stateService.setActiveChatElder(recipientId, elder.getId());
+                            log.info("🔗 [ACTIVE_CHAT] Установлен активный чат для userId={} на заявку {}", recipientId, elder.getId());
 
                             UniversalResponse response = new UniversalResponse(
                                     "✅ **Сообщение отправлено!**\n\n" +
